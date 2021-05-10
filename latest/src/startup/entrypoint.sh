901112b41daf0a1767538982a9f99bb773be9338
Artur Neumann <artur@jankaritech.com>
2021-05-10 11:52:57 +0545

    wait for license and retry to install squish & run tests

--- latest/src/startup/entrypoint.sh
+++ latest/src/startup/entrypoint.sh
@@ -3,7 +3,32 @@ export USER=headless
 
 /dockerstartup/vnc_startup.sh &
 
-/opt/squish.run unattended=1 ide=0 targetdir=${HOME}/squish licensekey=$LICENSEKEY
+#maximum time to wait for licenses (before installation of squish + before running tests)
+runtime="30 minute"
+endtime=$(date -ud "$runtime" +%s)
+
+result=1
+echo "installing squish"
+
+# retry installing squish if there is an issue to connect the license server
+while [[ $result -ne 0 ]]
+do
+  if [[ $(date -u +%s) -gt $endtime ]]
+  then
+    echo "timeout waiting for license server"
+    exit 1
+  fi
+
+  /opt/squish.run unattended=1 ide=0 targetdir=${HOME}/squish licensekey=$LICENSEKEY
+  result=$?
+
+  if [[ $result -ne 0 ]]
+  then
+    echo "sleeping waiting for license server"
+    sleep $((1 + $RANDOM % 30))
+  fi
+done
+
 
 cp ${HOME}/squish/etc/paths.ini ${HOME}/squish/etc/paths.ini-backup
 cp /dockerstartup/paths.ini ${HOME}/squish/etc/
@@ -13,4 +38,25 @@ cp ${SERVER_INI} ${HOME}/.squish/ver1/server.ini
 
 /home/headless/squish/bin/squishserver &
 
-~/squish/bin/squishrunner --testsuite ${CLIENT_REPO}/test/gui/ ${SQUISH_PARAMETERS} --exitCodeOnFail 1
+# squishrunner seems to wait itself for a license, but sometimes fails with error 37
+# error code that is returned by the squish runner if there is an issue with the license
+LICENSE_ERROR_RESULT_CODE=37
+result=LICENSE_ERROR_RESULT_CODE
+echo "starting tests"
+while true
+do
+  if [[ $(date -u +%s) -gt $endtime ]]
+  then
+    echo "timeout waiting for available license"
+    exit 1
+  fi
+  ~/squish/bin/squishrunner --testsuite ${CLIENT_REPO}/test/gui/ ${SQUISH_PARAMETERS} --exitCodeOnFail 1
+  result=$?
+  if [[ $result -eq LICENSE_ERROR_RESULT_CODE ]]
+  then
+    echo "sleeping waiting for available license"
+    sleep $((1 + $RANDOM % 30))
+  else
+    exit $result
+  fi
+done
