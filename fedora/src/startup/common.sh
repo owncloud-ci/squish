7f74f897f53a5693c98ac58829069c60b5dbc014
Saw-jan <saw.jan.grg3e@gmail.com>
2025-08-25 09:56:57 +0545

    feat: validate ownbuild qt with squish qt versions
    Signed-off-by: Saw-jan <saw.jan.grg3e@gmail.com>

--- fedora/src/startup/common.sh
+++ fedora/src/startup/common.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
+# DBUS_ENV_FILE is used in entrypoint.sh and vnc_startup.sh files
 DBUS_ENV_FILE=/tmp/dbus_env
+SQUISH_INSTALL_DIR="${HOME}/squish"
+SQUISH_INSTALL_LOG="${HOME}/squish-installation.log"
+
+function install_squish() {
+    echo "[SQUISH] Installing squish..."
+    echo "[SQUISH] Installation report: ${SQUISH_INSTALL_LOG}"
+
+    /opt/squish.run unattended=1 ide=0 doc=0 examples=0 targetdir="${SQUISH_INSTALL_DIR}" licensekey="${LICENSEKEY}" >>"${SQUISH_INSTALL_LOG}" 2>&1
+    result=$?
+
+    if [[ $result -ne 0 ]]; then
+        echo "[SQUISH] Failed to install squish."
+        cat "$SQUISH_INSTALL_LOG"
+        if [ -f "$HOME/squish/SquishConfig.log" ]; then
+            echo "-----------------------------------------"
+            cat "$HOME/squish/SquishConfig.log"
+        fi
+        return $result
+    fi
+}
