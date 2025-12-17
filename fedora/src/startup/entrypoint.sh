a7adbf09489e748c4a55c3566a31fff4bde4bed0
Prajwol Amatya <prajwolamatya11@gmail.com>
2025-12-17 16:11:42 +0545

    add env to skip test run
    Signed-off-by: Prajwol Amatya <prajwolamatya11@gmail.com>

--- fedora/src/startup/entrypoint.sh
+++ fedora/src/startup/entrypoint.sh
@@ -90,22 +90,6 @@ endtime=$(date -ud "$runtime" +%s)
 # start squishserver
 (${HOME}/squish/bin/squishserver >>"${GUI_TEST_REPORT_DIR}"/serverlog.log 2>&1) &
 
-# squishrunner waits itself for a license to become available, but fails with error 37 if it cannot connect to the license server
-LICENSE_ERROR_RESULT_CODE=37
-result=0
-echo "[SQUISH] Starting tests..."
-while true; do
-  if [[ $(date -u +%s) -gt $endtime ]]; then
-    echo "[SQUISH] Timeout waiting for license server"
-    exit 1
-  fi
-
-  ${HOME}/squish/bin/squishrunner ${SQUISH_PARAMETERS} --reportgen stdout --exitCodeOnFail 1
-  result=$?
-  if [[ $result -eq $LICENSE_ERROR_RESULT_CODE ]]; then
-    echo "[SQUISH] Waiting for license server"
-    sleep $((1 + $RANDOM % 30))
-  else
-    exit $result
-  fi
-done
+if [ "$SKIP_TEST_RUN" == "false" ]; then
+  "${STARTUPDIR}"/run_test.sh
+fi
